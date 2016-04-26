7b78570ecc7042c4f4fc9da81fdfddeea0214109
Kritarth Upadhyay <KUpadhyay@walmartlabs.com>
2016-04-26 12:47:41 -0700

    adding header support

---
+++ README.md
@@ -0,0 +1,22 @@
+# HTTP head ping service for watchmen
+
+https://github.com/iloire/WatchMen
+
+## History
+
+**0.2.0**
+
+- Check for custom status code (defaults to 200)
+- Add tests
+
+**0.1.0**
+
+- Add support for pingService options
+
+**0.0.2**
+
+- Add timeout
+
+**0.0.1**
+
+- Initial release
---
+++ index.js
@@ -0,0 +1,44 @@
+var request = require('request');
+
+function PingService() {
+}
+
+exports = module.exports = PingService;
+
+PingService.prototype.ping = function (service, callback) {
+  var startTime = +new Date();
+  var options = {
+    method: 'HEAD',
+    uri: service.url,
+    headers:service.header,
+    timeout: service.timeout
+  };
+
+  var expectedStatusCode = 200;
+
+  var serviceOptions = (service.pingServiceOptions && service.pingServiceOptions['http-head']) || {};
+  if (serviceOptions.statusCode && serviceOptions.statusCode.value) {
+    expectedStatusCode = parseInt(serviceOptions.statusCode.value, 10);
+  }
+
+  request.get(options, function (error, response, body) {
+    if (error) {
+      return callback(error, body, response, +new Date() - startTime);
+    }
+    if (response && response.statusCode != expectedStatusCode) {
+      var errMsg = 'Invalid status code. Found: ' + response.statusCode +
+          '. Expected: ' + expectedStatusCode;
+      return callback(errMsg, body, response, +new Date() - startTime);
+    }
+    callback(null, body, response, +new Date() - startTime);
+  });
+};
+
+PingService.prototype.getDefaultOptions = function () {
+  return {
+    'statusCode': {
+      descr: 'Expected status code (defaults to 200)',
+      required: false
+    }
+  };
+};
